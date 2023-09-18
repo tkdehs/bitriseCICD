fd27a06ab054f11894238921fe36664adc89614f
Sangdon Kim <107008333+sangdon-kim-switchwon@users.noreply.github.com>
2023-09-18 10:17:15 +0900

    Initial Commit

---
+++ bitriseCICD/bitriseCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitriseCICDApp.swift
+//  bitriseCICD
+//
+//  Created by PNX on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct bitriseCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
